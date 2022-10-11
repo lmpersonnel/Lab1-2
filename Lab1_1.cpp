3a9c80a42bf7eea55e4410f2745a8064b26db011
PavelSulimov <114027850+lmpersonnel@users.noreply.github.com>
2022-10-11 19:12:13 +0300

    Add files via upload

--- Lab1_1.cpp
+++ Lab1_1.cpp
@@ -1,25 +1,23 @@
 #include<stdio.h>
-#include<locale.h>
 int main()
 {
-	setlocale(LC_ALL, "Rus");
 	int r, l, h;
 	float pi = 3.1415926, S1, S2, V;
-		printf("Радиус равен ");
+		printf("Radius is ");
 	scanf_s("%d", &r);
-		printf("Боковое ребро равно ");
+		printf("L is ");
 	scanf_s("%d", &l);
-		printf("Высота равна ");
+		printf("H is ");
 	scanf_s("%d", &h);
 	if (r <= 0 || l <= 0 || h <= 0)
 	{
-		printf("Введите только натуральные значения!");
+		printf("Enter natural numbers!");
 		return 0;
 	}
 	S1 = pi * r * l;
 	S2 = pi * r * (r + l);
 	V = pi * r * r * h / 3;
-		printf("Площадь боковой поверхности равна %.3f\n", S1);
-		printf("Полная площадь равна %.3f\n", S2);
-		printf("Объем равен %.3f\n", V);
+		printf("Side surfuce area is %.3f\n", S1);
+		printf("Full area is %.3f\n", S2);
+		printf("Volume is %.3f\n", V);
 }
